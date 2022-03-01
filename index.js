4c79551e3cc3bdbf310e95c72c3185ee38149a9c
Eliezer Chaves <chaves.eliezer@outlook.com>
2022-03-01 19:20:56 -0300

    Valores padrões de input

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ function enviar(){
     var mensagem = document.getElementById('floatingTextarea').value;
 
     var url = "https://api.whatsapp.com/send?phone=+55" + ddd + telefone + "&text=" + mensagem
-    console.log(url)
-    console.log(ddd)
-    console.log(telefone)
-    console.log(mensagem)
+    
+
+
     window.open(url, '_blank')
+
+    ddd.value = "";
+    telefone.value = "";
+    mensagem.value = "";
 }
 
 
