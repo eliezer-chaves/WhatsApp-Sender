20b44274a91efbbbfdd22f74d92efbb774092da9
Eliezer Chaves <chaves.eliezer@outlook.com>
2022-03-01 19:54:37 -0300

    Tratamento de strings

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ function enviar(){
     var ddd = document.getElementById('ddd').value;
     var telefone = document.getElementById('telefone').value;
     var mensagem = document.getElementById('floatingTextarea').value;
+    var texto = mensagem.split(' ').join('%20')
+    
+    console.log(texto)
 
-    var url = "https://api.whatsapp.com/send?phone=+55" + ddd + telefone + "&text=" + mensagem
+    var url = "https://api.whatsapp.com/send?phone=+55" + ddd + telefone + "&text=" + texto
     
 
-
-    window.open(url, '_blank')
+    window.open(url)
 
     ddd.value = "";
     telefone.value = "";
