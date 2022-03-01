eed13613c1f492244ffbe648befc72e31a9998ec
Eliezer Chaves <chaves.eliezer@outlook.com>
2022-03-01 19:11:20 -0300

    Função JS para enviar mensagme

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-console.log("Hello");
+function enviar(){
+    var ddd = document.getElementById('ddd').value;
+    var telefone = document.getElementById('telefone').value;
+    var mensagem = document.getElementById('floatingTextarea').value;
 
-console.log("Hello");
+    var url = "https://api.whatsapp.com/send?phone=+55" + ddd + telefone + "&text=" + mensagem
+    console.log(url)
+    console.log(ddd)
+    console.log(telefone)
+    console.log(mensagem)
+    window.open(url, '_blank')
+}
+
+
