7edf9dd84f7179eb85c26fe543c47486fbdcd109
Eliezer Chaves <chaves.eliezer@outlook.com>
2022-03-01 22:40:03 -0300

    Mudança do scrpit

--- index.js
+++ index.js
@@ -1,16 +1,1 @@
-function enviarMensagem(){
-    var ddd = document.getElementById('ddd').value;
-    var telefone = document.getElementById('telefone').value;
-    var mensagem = document.getElementById('floatingTextarea').value;
-    var texto = mensagem.split(' ').join('%20')
-    
-    var url = "https://wa.me/+55"+ ddd + telefone + "?text="+ texto
-
-    window.open(url)
-
-    ddd.value = "";
-    telefone.value = "";
-    mensagem.value = "";
-}
-
 
